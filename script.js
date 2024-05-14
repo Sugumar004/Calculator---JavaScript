83b742e6dfba76e0f98dc8de63b5b5ffb230e571
Sugumar004 <sugumurugan004@gmail.com>
2024-05-14 16:19:16 +0530

    commited sucessfully

---
+++ script.js
@@ -0,0 +1,25 @@
+
+let outputScreen = document.getElementById("inp")
+
+function display(num){
+
+    outputScreen.value += num;
+
+}
+
+function calculate(){
+    try {
+        outputScreen.value = eval(outputScreen.value)
+    } catch (err) {
+        alert("Invalid")
+    }
+}
+
+function cle(){
+
+    outputScreen.value = ""
+}
+
+function del(){
+    outputScreen.value = outputScreen.value.slice(0,-1)
+}
